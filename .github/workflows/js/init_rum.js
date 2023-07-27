172203391650addf35be07eb02316bc87290da1a
wgt153783 <tbp153783@gmail.com>
2023-07-27 17:37:22 +0800

    Delete .github/workflows directory

--- .github/workflows/js/init_rum.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function () {
-  elasticApm.init({
-    serviceName: window.location.hostname.replace(/\./g,'-'),
-    serverUrl: 'https://lalala.jiabavee.com:8200',
-    serverUrlPrefix: '/intake/v2/api/events'
-  })
-});
-
-
